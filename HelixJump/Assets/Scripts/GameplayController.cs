f9dea7d500084508f300b1d2d26316502ce634b7
Maeva L <maeva.lauzier@gmail.com>
2019-02-27 20:14:54 +0100

    First shot

---
+++ HelixJump/Assets/Scripts/GameplayController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayController : MonoBehaviour {
+    public float speed = 100f;
+
+    private void OnMouseDrag()
+    {
+        float rotationX = Input.GetAxis("Mouse X") * Mathf.Deg2Rad * speed;
+        transform.Rotate(new Vector3(0,1,0) * -rotationX);
+    }
+}
